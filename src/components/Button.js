5bc9b292ec79091a41792d6dd93cbcfdc4f72fd0
Mahoney96 <cmahoney966@gmail.com>
2021-03-03 10:25:49 -0800

    initial dependency download

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class button extends Component {
+    render() {
+        
+    }
+}
+
+
+export default Button;
+
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import {
+    Switch as Switch,
+    Route as Route,
+    Router as Router,
+
+} from 'react-router-dom';
+
+
+
+export default (
+    <Switch>
+        <Route component={}/>
+        <Route component={}/>
+        <Route component={}/>
+    </Switch>
+)
